921c84d51a35c2eead004c846d81db93393c5b56
jiangrs <jiangrs@s-ec.com>
2017-05-29 15:45:11 +0800

    Soft raid supported(mdadm)

--- start-container.bash
+++ start-container.bash
@@ -2,18 +2,18 @@
 
 DOCKER_REP="/docker/repository"
 
-mdadm -Dsv 2>/dev/null >/dev/null
-pvscan 2>&1 >/dev/null && vgscan 2>&1 >/dev/null && lvscan 2>&1 >/dev/null
+sudo mdadm -Dsv 2>/dev/null >/dev/null
+sudo pvscan 2>&1 >/dev/null && sudo vgscan 2>&1 >/dev/null && sudo lvscan 2>&1 >/dev/null
 
-VG_RAID="$(pvscan  | grep '/dev/md' | awk '{print $4}')"
-[ "x$VG_RAID" != "x" ] && LV_RAID="$(lvscan 2>/dev/null | grep "${VG_RAID}" | awk -F\' '{print $2}')"
+VG_RAID="$(sudo pvscan  | grep '/dev/md' | awk '{print $4}')"
+[ "x$VG_RAID" != "x" ] && LV_RAID="$(sudo lvscan 2>/dev/null | grep "${VG_RAID}" | awk -F\' '{print $2}')"
 mkdir -p ${DOCKER_REP}
 
-[ "x$LV_RAID" != "x" ] && mount ${LV_RAID} ${DOCKER_REP}
+[ "x$LV_RAID" != "x" ] && sudo mount ${LV_RAID} ${DOCKER_REP}
 
 # ToDo: mount all points
 
 ${NEXUS_HOME}/bin/nexus run
 
-umount "${DOCKER_REP}" 2>/dev/null
+sudo umount "${DOCKER_REP}" 2>/dev/null
 
