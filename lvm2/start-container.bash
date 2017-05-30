c3260d3e49346c3e2aa8aeacd6539fca4225555f
jiangrs <jiangrs@s-ec.com>
2017-05-30 11:19:44 +0800

    lvm2 support test passed

--- lvm2/start-container.bash
+++ lvm2/start-container.bash
@@ -1,24 +1,13 @@
 #!/bin/sh
 
+# docker run -it --rm --privileged -v /dev/vgraid/lvol0:/dev/nexus --name nexus deepsecs/nexus3_lvm
+
 # ENV NEXUS_VOLUMN="/dev/nexus"
 
-sudo mount ${NEXUS_VOLUMN} /mnt
-
-if [ $? -eq 0 ]; then
-
-  if [ ! -d /mnt/${NEXUS_DATA} ]; then
-    # nexus directory not exist, create and copy from $NEXUS_DATA
-    sudo cp ${NEXUS_DATA} /mnt -R
-    sudo chown nexus:nexus /mnt${NEXUS_DATA}/* -R
-  fi
-
-  sudo mount -o bind /mnt${NEXUS_DATA} ${NEXUS_DATA}
-
-fi
-
-sudo chown -R nexus:nexus ${NEXUS_DATA}
+sudo mount ${NEXUS_VOLUMN} /mnt && \
+sudo mount -o bind /mnt/${NEXUS_DATA} ${NEXUS_DATA}
 
 ${NEXUS_HOME}/bin/nexus run
 
-sudo umount /mnt ${NEXUS_DATA}
+sudo umount ${NEXUS_DATA} /mnt
 
